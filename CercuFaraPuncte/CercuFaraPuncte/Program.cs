bcf36467d5ad6823bf74156397b3b93f359da9a2
Vinter Andrada <vinterandrada08@gmail.com>
2022-03-21 16:58:34 +0200

    cerc cu centru q, raza maxima, fata puncte

---
+++ CercuFaraPuncte/CercuFaraPuncte/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CercuFaraPuncte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
