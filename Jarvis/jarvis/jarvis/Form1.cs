614216d3d3eb686715c3ba8da48424573ab82547
andraaa0802 <vinterandrada08@gmail.com>
2022-05-18 00:47:09 +0300

    jarvis

---
+++ Jarvis/jarvis/jarvis/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jarvis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Red, 3);
+            Pen pen2 = new Pen(Color.Black, 3);
+
+            Random rand = new Random();
+            int n = rand.Next(5, 50);
+
+            PointF[] m = new PointF[n];
+            float ymax = float.MinValue;
+            float xmin = float.MaxValue;
+            float xa = 0, ya = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                m[i].X = rand.Next(50, panel1.Width - 50);
+                m[i].Y = rand.Next(50, panel1.Height - 50);
+                g.DrawEllipse(pen2, m[i].X, m[i].Y, 1.5F, 1.5F);
+                if (m[i].Y > ymax)
+                {
+                    ymax = m[i].Y;
+                    xa = m[i].X;
+                    ya = m[i].Y;
+                }
+                else if (m[i].Y == ymax)
+                {
+                    if (m[i].X < xmin)
+                    {
+                        xmin = m[i].X;
+                        xa = m[i].X;
+                        ya = m[i].Y;
+                    }
+                }
+            }
+
+            int k = 0;
+            bool valid = true;
+            float xk = xa;
+            float yk = ya;
+
+            while (valid)
+            {
+                float xp = m[k].X;
+                float yp = m[k].Y;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (determinant(xk, yk, xp, yp, m[i].X, m[i].Y) > 0)
+                    {
+                        xp = m[i].X;
+                        yp = m[i].Y;
+                    }
+                }
+                if (xp != xa && yp != ya)
+                {
+                    g.DrawLine(pen, xk, yk, xp, yp);
+                    Thread.Sleep(100);
+                    k++;
+                    xk = xp;
+                    yk = yp;
+                }
+                else
+                {
+                    g.DrawLine(pen, xk, yk, xp, yp);
+                    valid = false;
+                }
+            }
+        }
+        private float determinant(float xp, float yp, float xq, float yq, float xr, float yr)
+        {
+            float d = xp * (yq - yr) + xq * (yr - yp) + xr * (yp - yq);
+            return d;
+        }
+    }
+}
