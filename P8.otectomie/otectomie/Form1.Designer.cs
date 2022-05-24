1c2e97269c72150676889c3254d88cdfccb27f94
Cristina-Andrada Vinter <vinter.cristinaandrada@student.uoradea.ro>
2022-05-24 17:42:40 +0300

    changes

--- P8.otectomie/otectomie/Form1.Designer.cs
+++ P8.otectomie/otectomie/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -40,24 +39,13 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(679, 47);
+            this.button1.Location = new System.Drawing.Point(679, 44);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 63);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Inchidere poligon";
+            this.button1.Text = "Inchidere + triangulare";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(679, 126);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(143, 69);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Triangulare";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -84,7 +72,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(679, 212);
+            this.button4.Location = new System.Drawing.Point(679, 213);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(143, 68);
             this.button4.TabIndex = 4;
@@ -122,7 +110,6 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -135,7 +122,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
